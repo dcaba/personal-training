bce536f0f491f571a4056211fbd61f9d1db14274
dcaba@github <dani.caba@gmail.com>
2015-11-19 23:20:51 +0100

    Chapter 6 completed

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,3 +1,12 @@
+def time
+	Time.now.strftime "%H:%M:%S" 
+end
+
+def say_hello(name,health=100)
+	"I'm #{name.capitalize} with health #{health} as of #{time}"
+end
+
+
 def print_players(players) 
 	players.each do |player|
         	case player[0]
@@ -16,17 +25,12 @@ def print_players(players)
 end
 
 
-players=[["larry",60],["curly",120],["moe",100],["shemp",90]]
+players=[["larry",60],["curly",120],["moe",100],["shemp",90],"dani"]
 
-puts "Players: \n"
-print_players(players)
-
-puts "Modifyig health..."
-players[1][1]=players[0][1]
-puts "Modified PLayers:"
-print_players(players)
-
-puts "Modifying Larry's health"
-players[0][1]=10
-puts "New modified players:"
-print_players(players)
+players.each do |player,health|
+	if !health.nil? then
+		puts say_hello(player,health) 
+	else 
+		puts say_hello player 
+	end
+end
