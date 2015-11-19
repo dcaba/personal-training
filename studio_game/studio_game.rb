a5e7fd4c36afe197428bc0cbaf41f445f12ff3d7
dcaba@github <dani.caba@gmail.com>
2015-11-19 22:34:58 +0100

    end of exercise 4

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,10 +1,32 @@
-name1="larry"
-health1=60
-puts "#{name1}'s health is #{health1/9.0}"
+def print_players(players) 
+	players.each do |player|
+        	case player[0]
+        		when "larry"
+                		puts "\t #{player[0].capitalize} has a health of #{player[1]}."
+        		when "curly"
+                		puts "\t #{player[0].upcase} has a health of #{player[1]}."
+			when "moe"
+                		puts "\t "+"#{player[0].capitalize} has a health of #{player[1]}.".center(40,"*")
+			when "shemp"
+                		puts "\t #{player[0].capitalize.ljust(20,".")} health #{player[1]}."
+        		else
+                		puts "\t #{player[0]} \n"
+        	end
+	end
+end
 
-players=["larry","curly","moe"]
+
+players=[["larry",60],["curly",120],["moe",100],["shemp",90]]
 
 puts "Players: \n"
-players.each do |player|
-	puts "\t #{player} \n"
-end
+print_players(players)
+
+puts "Modifyig health..."
+players[1][1]=players[0][1]
+puts "Modified PLayers:"
+print_players(players)
+
+puts "Modifying Larry's health"
+players[0][1]=10
+puts "New modified players:"
+print_players(players)
