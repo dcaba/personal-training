af744ac225266fe82b47a60902572e0f091828d6
dcaba@github <dani.caba@gmail.com>
2015-12-08 12:10:07 +0100

    chapter 10 completed

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -31,10 +31,15 @@ attr_accessor :name
 end
 
 class Game
-attr_accessor :players,:name
+attr_accessor :players,:title
 
 def initialize(name)
-	@name = name
+	@title = name
+	@players = Array.new
+end
+
+def add_player(player)
+	@players << player
 end
 
 def import_players(players_import)
@@ -44,9 +49,7 @@ end
 def print_players(scope="all") 
 	case scope
 	when "all"
-		puts "*************"
-		puts "Players information"
-		puts "*************"
+		puts "Players information".center(80,"*")
 		@players.each do |player|
        			case player.name.downcase
        			when "larry"
@@ -62,39 +65,39 @@ def print_players(scope="all")
        			end
 		end 
 	when "health" 
-		puts "*************"
-		puts "Health report"
-		puts "*************"
+		puts "Health report".center(80,"*")
 		@players.each do |player|
-			puts player.health
+			puts "\t#{player.health}"
 		end
 	end
 end
 
 def play
+puts "Now we are playing with #{title} and #{players.size} players".center(80,"*")
 @players.each do |player|
         if (rand(1..2)==1)
-	        puts "---user #{player.name} was lucky... time to play with it!"
-		puts "Before playing: #{player}"
+	        puts "-user #{player.name} was lucky... time to play with it!"
+		puts "-Before playing: #{player}"
                 rand(1..10).times do
                         case rand(1..2)
                                 when 1
-                                 puts player.blam
+                                 puts "\t"+player.blam
                                 else
-                                 puts player.w00t
+                                 puts "\t"+player.w00t
                         end
                 end
-		puts "After playing: #{player}"
+		puts "-After playing: #{player}"
         end
 end
 end
 
 end
 
-def time
-        Time.now.strftime "%H:%M:%S"
-end
-
+def initialize_program
+welcome_message="Game starting at #{time}"
+puts "".center(80,"#")
+puts welcome_message.center(80,"#")
+puts "".center(80,"#")
 
 players_data=[["larry",60],["curly",120],["moe",100],["shemp",90],["dani"]]
 players = Array.new
@@ -104,27 +107,48 @@ players_data.each do |name,health|
 	players << player
 	puts "player has been processed: #{player}"
 end
+return players
+end
+
+
+def finish_program
+welcome_message="Game finished at #{time}"
+puts "".center(80,"#")
+puts welcome_message.center(80,"#")
+puts "".center(80,"#")
+end
+
+def time
+        Time.now.strftime "%H:%M:%S"
+end
+
+#
+# MAIN
+#
+players = initialize_program
 
 my_game = Game.new "myGame!"
 my_game.import_players(players)
-
 puts "Players have been imported. There are #{my_game.players.size} players in the game"
 
-puts "Game title: #{my_game.name}"
+puts "Adding a random player"
+my_game.add_player(Player.new)
+puts "Done"
+
+puts "Game title: #{my_game.title}"
 my_game.print_players
 my_game.print_players("health")
 
 my_game.play
 
-my_game.print_players
-my_game.print_players("health")
+#my_game.print_players
+#my_game.print_players("health")
+#players.reject! do |player|
+#	player.name.downcase == "curly"
+#end
+#players << Player.new("ShemP2")
+#my_game.print_players
+#my_game.import_players(players)
+#my_game.print_players
 
-players.reject! do |player|
-	player.name.downcase == "curly"
-end
-
-players << Player.new("ShemP2")
-
-my_game.print_players
-my_game.import_players(players)
-my_game.print_players
+finish_program
