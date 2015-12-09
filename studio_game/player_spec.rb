26b243320f047c96ea3e0e2bec8afddbcba1fdef
dcaba@github <dani.caba@gmail.com>
2015-12-09 19:14:21 +0100

    12-2-11

--- studio_game/player_spec.rb
+++ studio_game/player_spec.rb
@@ -6,8 +6,24 @@ describe Player do
 	end
 
 	it "has a capitalized name" do
-		expect(@player.name).to eq("Dani")
+		expect(@player.name).to be == "Dani"
 	end
+
+	it "has an initial health" do
+		expect(@player.health).to be > 0
+	end
+
+	it "has a string representation" do
+		expect(@player.to_s).not_to be nil
+	end
+
+	it "computes a score as the sum of its health and length of name" do
+		expect(@player.score).to be == "Dani".size + @player.health 
+	end
+
+	it "increases health by 15 when w00ted"
+
+	it "decreases health by 10 when blammed"
 
 end
 
