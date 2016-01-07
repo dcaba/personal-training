d9ab452ef23bc5de4d8946a3704e3b79a62c9919
dcaba@github <dani.caba@gmail.com>
2016-01-07 09:07:06 +0100

    ch21-beforeLoadedDie

--- studio_game/die.rb
+++ studio_game/die.rb
@@ -1,10 +1,15 @@
+require_relative 'auditable.rb'
+
 class Die
+	include Auditable
 	attr_reader :number
 	def initialize
 		roll
 	end
 	def roll
 		@number=rand(1..6)
+		audit
+		return @number
 	end
 end
 
---
+++ studio_game/loaded_die.rb
@@ -0,0 +1,12 @@
+require_relative 'auditable'
+
+loaded_nums=[1,1,2,5,6,6]
+class LoadedDie
+	include Auditable
+	attr_reader :number
+	def roll
+		@number=loaded_nums.sample
+		audit
+		@number
+	end
+end
