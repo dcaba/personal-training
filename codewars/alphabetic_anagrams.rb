6fdc0b579d178371c4a6b4f8e3dd698cf7753eb1
daniel-caballero <daniel.caballero@schibsted.com>
2017-07-05 17:04:21 +0200

    first brute force completed

--- codewars/alphabetic_anagrams.rb
+++ codewars/alphabetic_anagrams.rb
@@ -1,17 +1,13 @@
 # https://www.codewars.com/kata/alphabetic-anagrams/train/ruby
 
 def create_combinations(word)
-	comb_tables = {'A' => ['A'],
-		'ABAB' => ['AABB', 'ABAB', 'ABBA', 'BAAB', 'BABA', 'BBAA'],
-		'AAAB' => ['AAAB', 'AABA', 'ABAA', 'BAAA'],
-		'BAAA' => ['AAAB', 'AABA', 'ABAA', 'BAAA']
-	}
-	return comb_tables[word]
+	result = Array[]
+	word.split("").permutation.to_a.uniq.sort.each {|elem| result << elem.join() }
+	return result
 
 end
 
 def listPosition(word)
 	combinations = create_combinations(word)
-	#test_values = {'A' => 1, 'ABAB' => 2, 'AAAB' => 1, 'BAAA' => 4, 'QUESTION' => 24572, 'BOOKKEEPER' => 10743}
 	return combinations.index(word) + 1
 end
