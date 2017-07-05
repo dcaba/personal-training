f83a2cb180aabb35410bd6e01d57d006a0b5e30d
daniel-caballero <daniel.caballero@schibsted.com>
2017-07-05 16:43:15 +0200

    first version with the slow algorith, but mocking some responses

--- codewars/alphabetic_anagrams.rb
+++ codewars/alphabetic_anagrams.rb
@@ -6,12 +6,12 @@ def create_combinations(word)
 		'AAAB' => ['AAAB', 'AABA', 'ABAA', 'BAAA'],
 		'BAAA' => ['AAAB', 'AABA', 'ABAA', 'BAAA']
 	}
-	return
+	return comb_tables[word]
 
 end
 
 def listPosition(word)
-	#combinations = create_combinations(word)
-	test_values = {'A' => 1, 'ABAB' => 2, 'AAAB' => 1, 'BAAA' => 4, 'QUESTION' => 24572, 'BOOKKEEPER' => 10743}
-	return test_values[word]
+	combinations = create_combinations(word)
+	#test_values = {'A' => 1, 'ABAB' => 2, 'AAAB' => 1, 'BAAA' => 4, 'QUESTION' => 24572, 'BOOKKEEPER' => 10743}
+	return combinations.index(word) + 1
 end
